b807b41acd0743134febb930e9e770a9a808e5f3
charaf <developermarghin@gmail.com>
2022-07-21 01:52:37 +0100

    part 1 ex 1.3 step 3

--- part 1/courseinfo/src/components/Content.js
+++ part 1/courseinfo/src/components/Content.js
@@ -1,11 +1,11 @@
 import { Part } from "./Part";
 
-export const Content = ({ parts, exercises }) => {
+export const Content = ({parts}) => {
   return (
     <div>
-      <Part part={parts.part1} exercice={exercises.exercises1} />
-      <Part part={parts.part2} exercice={exercises.exercises2} />
-      <Part part={parts.part3} exercice={exercises.exercises3} />
+      <Part part={parts.part1.name} exercice={parts.part1.exercises} />
+      <Part part={parts.part2.name} exercice={parts.part2.exercises} />
+      <Part part={parts.part3.name} exercice={parts.part3.exercises} />
     </div>
   );
 };
