dd9819a7413ff978c435f012c30dc707a0da0d4f
charaf <developermarghin@gmail.com>
2022-07-22 11:57:09 +0100

    part 1 | 1.10 unicafe step5

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button } from "./components/Button";
-import { Statistics } from "./components/Statistics";
-
+import { StatisticLine } from "./components/StatisticLine";
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -17,7 +16,18 @@ const App = () => {
         <Button text="bad" setRate={setBad} />
       </div>
       <h1>Statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {good + neutral + bad > 0 ? (
+        <div>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+          <StatisticLine text="positive" value={(good / (good + neutral + bad)) * 100 + ' %'} />{" "}
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
