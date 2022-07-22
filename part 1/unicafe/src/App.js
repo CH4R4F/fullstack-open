e300ccbc324630369a5520d09cd3f516d906ee5c
charaf <developermarghin@gmail.com>
2022-07-22 12:00:51 +0100

    part 1 | 1.11 unicafe step6

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -17,14 +17,14 @@ const App = () => {
       </div>
       <h1>Statistics</h1>
       {good + neutral + bad > 0 ? (
-        <div>
+        <table>
           <StatisticLine text="good" value={good} />
           <StatisticLine text="neutral" value={neutral} />
           <StatisticLine text="bad" value={bad} />
           <StatisticLine text="all" value={good + neutral + bad} />
           <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
           <StatisticLine text="positive" value={(good / (good + neutral + bad)) * 100 + ' %'} />{" "}
-        </div>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
