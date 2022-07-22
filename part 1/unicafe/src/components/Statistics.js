77fd6f09d41ba2a8484157c9d4ec9e12416f5c7f
charaf <developermarghin@gmail.com>
2022-07-22 11:43:33 +0100

    part 1 | 1.7 unicafe step2

--- part 1/unicafe/src/components/Statistics.js
+++ part 1/unicafe/src/components/Statistics.js
@@ -1,9 +1,13 @@
 export const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad;
   return (
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {good / total}</p>
     </div>
   );
 };
