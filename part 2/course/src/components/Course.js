3b0956092480db422f53cc2813caf720915d9fe4
charaf <developermarghin@gmail.com>
2022-07-23 23:07:30 +0100

    2.2: Course information step7

--- part 2/course/src/components/Course.js
+++ part 2/course/src/components/Course.js
@@ -5,10 +5,13 @@ export const Course = ({ course }) => {
   return (
     <div>
       <Header text={course.name} />
-      <ul style={{listStyle: 'none'}}>
+      <ul style={{ listStyle: "none" }}>
         {course.parts.map((part) => (
           <Part key={part.id} name={part.name} exercises={part.exercises} />
         ))}
+        <li>
+          <b>total of {course.parts.reduce((acc, curr) => acc + curr.exercises, 0)} exercises</b>
+        </li>
       </ul>
     </div>
   );
