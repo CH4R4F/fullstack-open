28fed9fba3b5244a05ba04518ad860bafbc2f97d
charaf <developermarghin@gmail.com>
2022-07-23 23:12:51 +0100

    2.4: Course information step9
    separate modules already done from the beginning of the course

--- part 2/course/src/components/Course.js
+++ part 2/course/src/components/Course.js
@@ -1,18 +1,24 @@
 import { Header } from "./Header";
 import { Part } from "./Part";
 
-export const Course = ({ course }) => {
+export const Course = ({ courses }) => {
   return (
     <div>
-      <Header text={course.name} />
-      <ul style={{ listStyle: "none" }}>
-        {course.parts.map((part) => (
-          <Part key={part.id} name={part.name} exercises={part.exercises} />
-        ))}
-        <li>
-          <b>total of {course.parts.reduce((acc, curr) => acc + curr.exercises, 0)} exercises</b>
-        </li>
-      </ul>
+      {courses.map((course) => (
+        <div>
+          <Header text={course.name} />
+          <ul style={{ listStyle: "none" }}>
+            {course.parts.map((part) => (
+              <Part key={part.id} name={part.name} exercises={part.exercises} />
+            ))}
+            <li>
+              <b>
+                total of {course.parts.reduce((acc, curr) => acc + curr.exercises, 0)} exercises
+              </b>
+            </li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
