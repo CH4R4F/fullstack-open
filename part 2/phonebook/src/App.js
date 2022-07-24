d3cc46b41920583acb134b1ef29f50aa5f923a52
charaf <developermarghin@gmail.com>
2022-07-24 11:42:40 +0100

    2.8: The Phonebook Step3

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,20 +1,27 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   function addPerson(e) {
     e.preventDefault();
     if (persons.some((person) => person.name.toUpperCase() === newName.toUpperCase())) {
       alert(`${newName} is already added to phonebook`);
-    } else {
-      const personName = {
-        name: newName,
-      };
-      setPersons(persons.concat(personName));
-      setNewName("");
+      return;
+    } else if (!newName || !newNumber) {
+      alert("You most fill all fields");
+      return;
     }
+
+    const personName = {
+      name: newName,
+      number: newNumber,
+    };
+    setPersons(persons.concat(personName));
+    setNewName("");
+    setNewNumber("");
   }
 
   return (
@@ -25,12 +32,18 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number:{" "}
+          <input type="number" value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person}>{person.name}</p>
+        <p key={person}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
